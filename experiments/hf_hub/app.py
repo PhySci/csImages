f4780173b532f9f573cfb0f222f362b5eef3af6b
PhySci <mushenokf@gmail.com>
2024-03-09 22:39:44 +0100

    Add negative prompt

--- experiments/hf_hub/app.py
+++ experiments/hf_hub/app.py
@@ -9,16 +9,21 @@ col1, col2 = st.columns(2)
 
 with col1:
     orig_file = st.file_uploader("Choose an original image", type="jpg")
-    prompt = st.text_area(label="prompt", value="Vibrant, cartoon-style cat astronaut, perfect for slot game art.")
-    strength = st.slider(label="strength", min_value=0.05, max_value=1.0)
-
+    prompt = st.text_area(label="prompt",
+                          value="Vibrant, cartoon-style cat astronaut, perfect for slot game art.")
+    negative_prompt = st.text_area(label="negative prompt",
+                                   value="Vibrant, cartoon-style cat astronaut, perfect for slot game art.")
+    strength = st.slider(label="strength",
+                         min_value=0.05,
+                         max_value=1.0)
 
 if orig_file is not None:
     with col2:
         payload = {"inputs":
                        {"image": base64.b64encode(orig_file.getvalue()).decode("utf8"),
                         "strength": strength,
-                        "prompt": prompt}
+                        "prompt": prompt,
+                        "negative_prompt": negative_prompt}
                    }
         API_URL = "https://juwk62dxv3rfpdxn.us-east-1.aws.endpoints.huggingface.cloud"
         headers = {
---
+++ experiments/nst/prepare_img.py
@@ -0,0 +1,4 @@
+import torchvision.models as models
+
+if __name__ == "__main__":
+    models.vgg19(pretrained=True)
